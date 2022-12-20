16bcbb74ecbf9cb7c0c4f68e9883a6e4f44ed370
Visnja Sretenovic <snjava1195@gmail.com>
2022-12-21 00:07:10 +0100

    Comments removed

--- frontend/src/pages/DaoSettings/GeneralDaoSettings/index.jsx
+++ frontend/src/pages/DaoSettings/GeneralDaoSettings/index.jsx
@@ -44,10 +44,6 @@ const GeneralDaoSettings = () => {
     dispatch(getFactory())
   }, [])
 
-  console.log('dao: ', dao)
-  console.log('formData: ', formData)
-  console.log('id: ', id)
-
   let daoRootAddr
   if (dao) {
     for (let i = 0; i < dao.allDAOs.length; i++) {
@@ -56,7 +52,6 @@ const GeneralDaoSettings = () => {
       }
     }
   }
-  console.log('daoRootAddr: ', daoRootAddr)
 
   async function setSettingsChanges(name, slug, description, id) {
     //const factory = getFactory()
@@ -64,13 +59,11 @@ const GeneralDaoSettings = () => {
       daoFactoryAbi,
       dao.factoryAddress[0]._address
     )
-    console.log('Factory address: ', dao.factoryAddress[0]._address)
     const daoAddresses = await daoFactoryContract.methods
       .getDeployedDAOs({})
       .call()
 
     let daoRootAddress
-    console.log('daoAddresses: ', daoAddresses)
     for (let i = 0; i < daoAddresses.daoAddr.length; i++) {
       if (i == id) {
         daoRootAddress = daoAddresses.daoAddr[i][1][0]._address
@@ -87,7 +80,6 @@ const GeneralDaoSettings = () => {
           withoutSignature: true,
         })
 
-      console.log(daoRootAddress)
       console.log('trx: ', trx)
       return Promise.resolve(trx)
     } catch (e) {
