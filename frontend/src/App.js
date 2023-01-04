5df3538593c42eb5dc70d03e9a824213282e19bf
Nemanja <roki94ftn@gmail.com>
2023-01-04 15:51:35 +0100

    Context for wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
+import { useState, useMemo } from 'react'
 import WalletContext from './context/walletContext'
 import Navigation from 'navigation'
 import './App.css'
 
 function App() {
+  const [wallet, setWallet] = useState({ address: '', balance: null })
+  const providerValue = useMemo(
+    () => ({ wallet, setWallet }),
+    [wallet, setWallet]
+  )
   return (
     <div className="container">
-      <WalletContext.Provider
-        value={{ address: 'Something', balance: '903193201' }}
-      >
+      <WalletContext.Provider value={providerValue}>
         <Navigation />
       </WalletContext.Provider>
     </div>
--- frontend/src/context/walletContext.js
+++ frontend/src/context/walletContext.js
@@ -1,8 +1,5 @@
 import { createContext } from 'react'
 
-const WalletContext = createContext({
-  address: '',
-  balance: '',
-})
+const WalletContext = createContext(null)
 
 export default WalletContext
