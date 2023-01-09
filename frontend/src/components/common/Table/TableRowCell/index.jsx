0ed8023502a4b9056fe0119a16cd0d37f53201f2
Nemanja <roki94ftn@gmail.com>
2023-01-09 16:18:45 +0100

    Balance page

--- frontend/src/components/common/Table/TableRowCell/index.jsx
+++ frontend/src/components/common/Table/TableRowCell/index.jsx
@@ -1,12 +1,71 @@
 import _ from 'lodash'
 import styles from './styles.module.sass'
+import { useLocation } from 'react-router-dom'
+import Button from '../../Button'
+import walletAvatar from 'static/svg/walletAvatar.svg'
+import { addressFormat } from '../../../../helpers/addressFormat'
 
 const TableRowCell = ({ item, column }) => {
   const value = _.get(item, column.key)
+  const location = useLocation()
+
+  const className =
+    item.status === 'Active' || item.status === 'Pending'
+      ? styles.darkBlueActive
+      : item.status === 'Canceled'
+      ? styles.darkBlueInactive
+      : item.status === 'Failed' || item.status === 'Expired'
+      ? styles.red
+      : item.status === 'Succeeded' || item.status === 'Queued'
+      ? styles.yellow
+      : styles.green
 
   return (
-    <div className={styles.tableCell} style={{ width: column.width }}>
-      {column.render ? column.render(column, item) : value}
+    <div style={{ width: column.width }}>
+      {column.key === 'voting' ? (
+        <div className={styles.rangeWrapper}>
+          <div className={styles.percentageRow}>
+            <p className={styles.yes}>20%</p>
+            <p className={styles.no}>80%</p>
+          </div>
+          <div className={styles.line}></div>
+        </div>
+      ) : column.key === 'date' ? (
+        <div className={styles.dateWrapper}>
+          <p className={styles.action}>Action in 22 days</p>
+          <p className={styles.date}>Jun 07, 2021, 17:22</p>
+        </div>
+      ) : column.key === 'unlockTokens' ? (
+        <Button style={'primaryBtn'} text={'Unlock'} />
+      ) : column.key === 'myvote' ? (
+        <div className={styles.rangeWrapper}>
+          <div className={styles.dateWrapper}>
+            <p className={styles.action}>For</p>
+            <p className={styles.date}>123 000.72</p>
+          </div>
+        </div>
+      ) : column.key === 'addresses' ? (
+        <div className={styles.addressWrapper}>
+          <img src={walletAvatar} alt={'address'} />
+          <p className={styles.address}>
+            {addressFormat(
+              '0:ce752d9d8fea9be1d760dd2ee2bb3b2d8d1c6459fdaf61561277672c84db11f2'
+            )}
+          </p>
+        </div>
+      ) : (
+        <p
+          className={
+            location.pathname === '/'
+              ? styles.tableCellLoading
+              : styles.tableCell
+          }
+        >
+          <span className={column.key === 'status' ? className : ''}>
+            {column.render ? column.render(column, item) : value}
+          </span>
+        </p>
+      )}
     </div>
   )
 }
