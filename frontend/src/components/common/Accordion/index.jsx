d953362a1494abfcd89d7a323704247f655c4a0a
Nemanja <roki94ftn@gmail.com>
2023-02-24 14:30:50 +0100

    Added accordions

--- frontend/src/components/common/Accordion/index.jsx
+++ frontend/src/components/common/Accordion/index.jsx
@@ -1,9 +1,52 @@
+import { useState } from 'react'
 import styles from './styles.module.sass'
+import accordionArrowBottom from 'static/svg/accordionArrowBottom.svg'
+import accordionArrowTop from 'static/svg/accordionArrowTop.svg'
 
-const Accordion = () => {
+const Accordion = ({ title, content }) => {
+  const [open, setOpen] = useState(false)
   return (
-    <div>
-      <p>sd</p>
+    <div className={styles.accordion}>
+      <div
+        className={styles.accordionTopContainer}
+        onClick={() => setOpen(!open)}
+      >
+        <p>{title}</p>
+        <img
+          src={open ? accordionArrowTop : accordionArrowBottom}
+          alt={'accordion arrow'}
+        />
+      </div>
+      <div
+        className={
+          open ? styles.accordionContent : styles.accordionContentHidden
+        }
+      >
+        <div className={styles.row}>
+          <p className={styles.info}>Type</p>
+          <p className={styles.value}>Value</p>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.info}>Original network</p>
+          <p className={styles.value}>Value</p>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.info}>Token contract address</p>
+          <p className={styles.value}>Value</p>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.info}>Ticker</p>
+          <p className={styles.value}>Value</p>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.info}>Token name</p>
+          <p className={styles.value}>Value</p>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.info}>Decimal places</p>
+          <p className={styles.value}>Value</p>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Proposal/index.jsx
+++ frontend/src/pages/Proposal/index.jsx
@@ -3,7 +3,9 @@ import ContentHeader from 'components/common/ContentHeader'
 import ProposalVotingCard from 'components/ProposalVotingCard'
 import BalanceProposalInfo from 'components/BalanceProposalInfo'
 import Button from 'components/common/Button'
+import Accordion from 'components/common/Accordion'
 import styles from './styles.module.sass'
+import linkIcon from 'static/svg/linkIcon.svg'
 
 const Subheading = ({ text }) => {
   return <h3 className={styles.subheading}>{text}</h3>
@@ -16,41 +18,82 @@ const Proposal = () => {
       <ContentHeader title={'#33 Proposal name'} />
       <div className={styles.proposal}>
         <div className={styles.contentWrapper}>
-          <Subheading text={'Voting'} />
           <div className={styles.content}>
+            <Subheading text={'Voting'} />
             <div className={styles.votingCardContainer}>
               <ProposalVotingCard heading={'For'} />
               <ProposalVotingCard heading={'Against'} />
             </div>
-            {/*<BalanceProposalInfo heading={'Proposal management'}>*/}
-            {/*  <Button style={'primaryBtn'} text={'Cancel proposal'} />*/}
-            {/*  <Button style={'lightBlueBtn'} text={'Execute proposal'} />*/}
-            {/*</BalanceProposalInfo>*/}
-
-            <BalanceProposalInfo heading={'Your vote'}>
-              <div className={styles.infoRow}>
-                <p className={styles.parameter}>Voting power</p>
-                <p className={styles.value}>2000000</p>
+            <Subheading text={'Timeline'} />
+            <Subheading text={'About proposal'} />
+            <div className={styles.aboutProposalContainer}>
+              <div className={styles.aboutSectionSubheading}>
+                <h4 className={styles.subheadingText}>Description</h4>
+                <div className={styles.linkWrapper}>
+                  <p className={styles.discussion}>Discussion</p>
+                  <img src={linkIcon} alt={'link-icon'} />
+                </div>
               </div>
-              <div className={styles.buttonsWrapper}>
-                <Button text={'Vote for'} style={'greenBtn'} />
-                <Button text={'Vote against'} style={'redBtn'} />
-              </div>
-              <div className={styles.infoRow}>
-                <p className={styles.parameter}>Vote weight</p>
-                <p className={styles.value}>11.65</p>
-              </div>
-              <Button
-                disabled={true}
-                style={'disabledBtn'}
-                text={'Unlock Tokens'}
-              />
-              <p className={styles.notice}>
-                Your tokens are locked. You can unlock them after voting to be
-                able to withdraw.
+              <p className={styles.descriptionContent}>
+                The general problem The current rules and social mechanics of
+                Wikipedia discourage people from contributing. The existing
+                community is much smaller than it potentially could be. Many
+                Wikipedia mechanics and bureaucratic procedures are easy to
+                abuse. That leads to a lack of trust between community members.
+                These problems lower user engagement and turn potentially very
+                experienced and seasoned professionals and knowledgeable
+                individuals away.
               </p>
-            </BalanceProposalInfo>
+              <h4 className={styles.subheadingText}>Actions</h4>
+              <Accordion title={'Title 1'} />
+              <Accordion title={'Title 2'} />
+              <h4 className={styles.subheadingText}>Periods</h4>
+              <div className={styles.row}>
+                <p className={styles.info}>Voting delay</p>
+                <p className={styles.value}>Value</p>
+              </div>
+              <div className={styles.row}>
+                <p className={styles.info}>Voting period</p>
+                <p className={styles.value}>Value</p>
+              </div>
+              <div className={styles.row}>
+                <p className={styles.info}>Time lock</p>
+                <p className={styles.value}>Value</p>
+              </div>
+              <div className={styles.row}>
+                <p className={styles.info}>Grace period</p>
+                <p className={styles.value}>Value</p>
+              </div>
+            </div>
           </div>
+
+          {/*<BalanceProposalInfo heading={'Proposal management'}>*/}
+          {/*  <Button style={'primaryBtn'} text={'Cancel proposal'} />*/}
+          {/*  <Button style={'lightBlueBtn'} text={'Execute proposal'} />*/}
+          {/*</BalanceProposalInfo>*/}
+          <BalanceProposalInfo heading={'Your vote'}>
+            <div className={styles.infoRow}>
+              <p className={styles.parameter}>Voting power</p>
+              <p className={styles.value}>2000000</p>
+            </div>
+            <div className={styles.buttonsWrapper}>
+              <Button text={'Vote for'} style={'greenBtn'} />
+              <Button text={'Vote against'} style={'redBtn'} />
+            </div>
+            <div className={styles.infoRow}>
+              <p className={styles.parameter}>Vote weight</p>
+              <p className={styles.value}>11.65</p>
+            </div>
+            <Button
+              disabled={true}
+              style={'disabledBtn'}
+              text={'Unlock Tokens'}
+            />
+            <p className={styles.notice}>
+              Your tokens are locked. You can unlock them after voting to be
+              able to withdraw.
+            </p>
+          </BalanceProposalInfo>
         </div>
       </div>
     </div>
