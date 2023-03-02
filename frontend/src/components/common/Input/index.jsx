9648614522729e129b979220ca1fff351aebd981
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2023-03-02 17:04:57 +0100

    Validator inplemented
    TODO: Refactor and implement slug duplication checking

--- frontend/src/components/common/Input/index.jsx
+++ frontend/src/components/common/Input/index.jsx
@@ -1,7 +1,11 @@
 import styles from './styles.module.sass'
 import { useForm } from 'react-hook-form'
 import linkIcon from 'static/svg/linkIcon.svg'
-import { validator, whatPage, styling } from 'helpers/formValidator'
+import {
+  inputValidator,
+  whatPage,
+  inputErrorSwitchStyle,
+} from 'helpers/formValidator'
 import Tooltip from 'components/common/Tooltip'
 
 const Input = ({
@@ -36,14 +40,14 @@ const Input = ({
     } else if (!value && registerInput !== 'ownerAddress') {
       return 'This is a required field *'
     } else if (registerInput === 'ownerAddress') {
-      return validator(value, 0, registerInput, false, null)
+      return inputValidator(value, 0, registerInput, false, null)
     } else if (page === 2) {
       let voting = registerInput === 'voting' ? true : false
-      return validator(value, page, hourOrDay, false, voting)
+      return inputValidator(value, page, hourOrDay, false, voting)
     } else if (id === 'daoSlug') {
-      return validator(value, page, registerInput, false)
+      return inputValidator(value, page, registerInput, false)
     } else {
-      return validator(value, page, registerInput, false)
+      return inputValidator(value, page, registerInput, false)
     }
   }
 
@@ -136,10 +140,10 @@ const Input = ({
       {buttons}
       <span
         style={{
-          position: styling(registerInput, 'position'),
-          bottom: styling(registerInput, 'bottom'),
-          marginTop: styling(registerInput, 'marginT'),
-          marginBottom: styling(registerInput, 'marginB'),
+          position: inputErrorSwitchStyle(registerInput, 'position'),
+          bottom: inputErrorSwitchStyle(registerInput, 'bottom'),
+          marginTop: inputErrorSwitchStyle(registerInput, 'marginT'),
+          marginBottom: inputErrorSwitchStyle(registerInput, 'marginB'),
           color: 'red',
           fontSize: '14px',
         }}
