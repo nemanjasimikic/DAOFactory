0da378cc1f5c2c660d792b12c84208915849dbfc
Kris diFire <65904004+KrisdiFire@users.noreply.github.com>
2023-03-06 12:22:50 +0100

    Update formValidator.js

--- frontend/src/helpers/formValidator.js
+++ frontend/src/helpers/formValidator.js
@@ -99,7 +99,7 @@ export const inputValidator = (
         return 'Error: Cannot be empty'
       } else if (parseInt(data) >= parseInt(extraData)) {
         error = true
-        return 'Error: Value cannot be lower than Min Stake'
+        return 'Error: Value cannot be higher than Min Stake'
       } else {
         error = false
       }
