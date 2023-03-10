6e3bf2d342687ee121c9e7a1f6d45fd8fca6d015
Nemanja <roki94ftn@gmail.com>
2023-03-10 16:01:50 +0100

    fixed inputs, textareas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,9 @@
-import { useState, useMemo } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ContextProvider } from './context/walletContext'
 import Navigation from 'navigation'
 import './App.css'
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 function App() {
-  //const [wallet, setWallet] = useState({ address: '', balance: null })
-  /*const providerValue = useMemo(
-    () => ({ wallet, setWallet }),
-    [wallet, setWallet]
-  )*/
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
